24bc8056fc83ea26c01e20924d943e40a39bb56a
Arthur Drabazha <jhorr2222@gmail.com>
2021-01-14 12:08:15 +0200

    Remove redundant annotations

--- src/main/java/com/arthurdrabazha/openmind/repository/UserRepository.java
+++ src/main/java/com/arthurdrabazha/openmind/repository/UserRepository.java
@@ -12,16 +12,18 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     User findUserByUsername(String username);
 
-    List<User> findUsersByCategoriesContains(List<Category> categories);
+    List<User> findUsersByCategoriesContains(Category categories);
 
     @Query(value = "SELECT old_password_digest " +
             "FROM user_passwords " +
             "WHERE user_id=:id",
             nativeQuery = true)
-    List<String> findAllUsedPasswordsForUser(@Param("id") Long id);
+
+    List<String> findAllUsedPasswordsForUser(Long id);
 
     @Query(value = "INSERT INTO user_passwords (user_id, old_password_digest)" +
             "VALUES user_id=:id, old_password_digest=:password",
             nativeQuery = true)
+
     void createUserUsedPassword(@Param("password") String passwordDigest, @Param("id") Long userId);
 }
