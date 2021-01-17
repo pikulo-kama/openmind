0cca37401dab888c7c903699e544552c8b3f397c
Arthur Drabazha <jhorr2222@gmail.com>
2021-01-17 02:03:37 +0200

    Simplify domain models and move mappings to repo

--- src/main/java/com/arthurdrabazha/openmind/model/Category.java
+++ src/main/java/com/arthurdrabazha/openmind/model/Category.java
@@ -5,18 +5,13 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotBlank;
-import java.util.List;
 
-@Entity
+@Entity(name = "categories")
 @Builder
 @Data
 @NoArgsConstructor
@@ -33,19 +28,4 @@ public class Category {
     @NotBlank
     private String description;
 
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(
-            name = "users_categories",
-            joinColumns = { @JoinColumn(name = "user_id") },
-            inverseJoinColumns = { @JoinColumn(name = "category_id") }
-    )
-    private List<User> users;
-
-    @ManyToMany(cascade = CascadeType.ALL)
-    @JoinTable(
-            name = "posts_categories",
-            joinColumns = { @JoinColumn(name = "post_id") },
-            inverseJoinColumns = { @JoinColumn(name = "category_id") }
-    )
-    private List<Post> posts;
 }
--- src/main/java/com/arthurdrabazha/openmind/repository/CategoryRepository.java
+++ src/main/java/com/arthurdrabazha/openmind/repository/CategoryRepository.java
@@ -1,18 +1,19 @@
 package com.arthurdrabazha.openmind.repository;
 
 import com.arthurdrabazha.openmind.model.Category;
-import com.arthurdrabazha.openmind.model.Post;
-import com.arthurdrabazha.openmind.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
+public interface CategoryRepository extends JpaRepository<Category, Short> {
 
-    List<Category> findAllByUsersContains(User user);
+    @Query(value = "SELECT category_id FROM users_categories WHERE user_id=:userId", nativeQuery = true)
+    List<Category> findAllByUsersContains(Long userId);
 
-    List<Category> getAllByPostsContains(Post post);
+    @Query(value = "SELECT category_id FROM posts_categories WHERE user_id=:postId", nativeQuery = true)
+    List<Category> getAllByPostsContains(Long postId);
 
 }
