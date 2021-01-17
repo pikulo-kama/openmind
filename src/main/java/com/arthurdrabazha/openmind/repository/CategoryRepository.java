9c3f9ba203be430ddb42ec5b0d86237d83d8ccbc
Arthur Drabazha <jhorr2222@gmail.com>
2021-01-17 02:12:43 +0200

    Remove unnecessary annotations and imports

--- src/main/java/com/arthurdrabazha/openmind/repository/CategoryRepository.java
+++ src/main/java/com/arthurdrabazha/openmind/repository/CategoryRepository.java
@@ -3,11 +3,9 @@ package com.arthurdrabazha.openmind.repository;
 import com.arthurdrabazha.openmind.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface CategoryRepository extends JpaRepository<Category, Short> {
 
     @Query(value = "SELECT category_id FROM users_categories WHERE user_id=:userId", nativeQuery = true)
--- src/main/java/com/arthurdrabazha/openmind/repository/PostRepository.java
+++ src/main/java/com/arthurdrabazha/openmind/repository/PostRepository.java
@@ -2,7 +2,6 @@ package com.arthurdrabazha.openmind.repository;
 
 import com.arthurdrabazha.openmind.model.Category;
 import com.arthurdrabazha.openmind.model.Post;
-import com.arthurdrabazha.openmind.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
