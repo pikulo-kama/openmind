f4f99a31b15ec41cb215e034f3925cda437d21e4
Arthur Drabazha <jhorr2222@gmail.com>
2021-01-20 19:19:00 +0200

    Add password encoder bean

--- src/main/java/com/arthurdrabazha/openmind/config/WebConfig.java
+++ src/main/java/com/arthurdrabazha/openmind/config/WebConfig.java
@@ -1,10 +1,18 @@
 package com.arthurdrabazha.openmind.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan
 public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public BCryptPasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
 }
