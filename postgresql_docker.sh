2540608174e5a0d7dc14fc2b5b9ab1dffc88aa92
Arthur Drabazha <jhorr2222@gmail.com>
2021-01-24 13:42:08 +0200

    Reformat script structure

--- postgresql_docker.sh
+++ postgresql_docker.sh
@@ -6,5 +6,9 @@ password=root
 database=openmind_db
 
 docker kill $container_name
-docker rm $container_name
-docker run --name $container_name -e POSTGRES_PASSWORD=$password -e POSTGRES_DB=$database -p $port:5432 -d postgres:13.1-alpine
+docker rm -f $container_name
+docker run --name $container_name \
+           -e POSTGRES_PASSWORD=$password \
+           -e POSTGRES_DB=$database \
+           -p $port:5432 \
+           -d postgres:13.1-alpine
