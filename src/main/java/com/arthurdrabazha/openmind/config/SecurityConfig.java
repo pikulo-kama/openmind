7b3984f973780db2f451c59aacf2122f1441ee03
Arthur Drabazha <jhorr2222@gmail.com>
2021-01-28 14:16:34 +0200

    Update security config

--- src/main/java/com/arthurdrabazha/openmind/config/SecurityConfig.java
+++ src/main/java/com/arthurdrabazha/openmind/config/SecurityConfig.java
@@ -1,16 +1,62 @@
 package com.arthurdrabazha.openmind.config;
 
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public AuthenticationManager getAuthManager() throws Exception {
+        return super.authenticationManager();
+    }
+
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.httpBasic().disable();
+        httpSecurity
+                .csrf()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/u/account", "/u/update-password", "/u/update")
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .permitAll()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/admin/**", "/u/activate/**", "/u/deactivate/**", "/u/delete/**")
+                .hasRole("ADMIN")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**")
+                .permitAll();
+    }
+
+    @Autowired
+    @SneakyThrows
+    public void configureGlobal(@Qualifier("userServiceImpl") UserDetailsService userDetailsService,
+                                AuthenticationManagerBuilder auth,
+                                PasswordEncoder passwordEncoder) {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
     }
 }
